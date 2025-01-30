fb9520973126393f929e8bfc9915f793486d86e8
huanyu <91276231+HuanyuL@users.noreply.github.com>
2025-01-30 16:01:50 +0100

    flatten submodules

---
+++ pymoveit2/pymoveit2/robots/ur.py
@@ -0,0 +1,22 @@
+from typing import List
+
+MOVE_GROUP_ARM: str = "ur_manipulator"
+
+
+def joint_names() -> List[str]:
+    return [
+        "shoulder_pan_joint",
+        "shoulder_lift_joint",
+        "elbow_joint",
+        "wrist_1_joint",
+        "wrist_2_joint",
+        "wrist_3_joint",
+    ]
+
+
+def base_link_name(prefix: str = "ur_") -> str:
+    return "base_link"
+
+
+def end_effector_name(prefix: str = "ur_") -> str:
+    return "tool0"
